fb5fe581c1a7161459e938a2be01911bff8463c2
Vladislav Sehtman <vlad96@hotmail.de>
2023-02-07 13:02:45 +0100

    added allowEmptyShould to match: "add-3rd-party-rules" branch.

--- src/test/java/com/devonfw/sample/archunit/ThirdPartyRulesTest.java
+++ src/test/java/com/devonfw/sample/archunit/ThirdPartyRulesTest.java
@@ -16,12 +16,14 @@ public class ThirdPartyRulesTest {
             noClasses()
             .should().dependOnClassesThat()
             .haveFullyQualifiedName(org.springframework.transaction.annotation.Transactional.class.getName())
-            .because("the use of JEE standard (javax.transaction.Transactional from javax.transaction:javax.transaction-api:1.2+) is encouraged.");
+            .because("the use of JEE standard (javax.transaction.Transactional from javax.transaction:javax.transaction-api:1.2+) is encouraged.")
+            .allowEmptyShould(true);
 
     @ArchTest
     static final ArchRule no_api_scoped_classes_should_access_transactional_annotation =
             noClasses()
             .that().resideInAPackage("..api..")
             .should().beAnnotatedWith(Transactional.class)
-            .because("the use of @Transactional in API is discouraged. Instead use it to annotate implementations.");
+            .because("the use of @Transactional in API is discouraged. Instead use it to annotate implementations.")
+            .allowEmptyShould(true);
 }
